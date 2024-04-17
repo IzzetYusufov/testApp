21951effab6b1d5c263df3d70331533891511d6b
Izzet <izzet.yusufov.dev@gmail.com>
2024-04-18 01:17:11 +0200

    resolve path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/IzzetYusufov/testApp/test",
+	base: "/testApp/",
 	plugins: [react()],
 })
